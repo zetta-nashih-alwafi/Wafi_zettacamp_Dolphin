ed378834eefcd4c43bf652360e0dbce0969966c0
zettacamp-muhammad-nashihalwafi <nashih.alwafi@zettacamp.pro>
2023-09-01 21:09:16 +0700

    [Initialize] Upload File JavaScript Day 6

---
+++ majorityElement_wafi.js
@@ -0,0 +1,47 @@
+
+/**
+ * write a function that returns the majority element.
+ * The majority element is the element that appears more than other element.
+ * READ EXAMPLE BELOW!
+
+
+
+ * You may assume that the majority element always exists in the array.
+
+ * Returns the majority element from the input array of integers.
+
+ * @param {number[]} nums - The input array of integers.
+ * @return {number} Returns the majority element.
+ */
+
+function majorityElement(nums) {
+  const setNums = new Set(nums)
+  let majorElement = 0, majorElementAmount = 0, findElement = 0
+
+  for (const num of setNums) {
+    nums.forEach((element) =>{
+      if (num === element) {
+        findElement ++
+      }
+    })
+
+    if (findElement > majorElementAmount) {
+      majorElementAmount = findElement
+      majorElement = num
+    }
+    findElement = 0
+  }
+  return majorElement;
+}
+
+console.log(majorityElement([1, 2, "i", "a", "i", 1]));
+console.log(majorityElement([1, 2, 1, 2, 3]));
+
+console.log(majorityElement([[3, 2], [3, 7], [3, 2]])); // Output: 3 
+console.log(majorityElement([2, 2, 1, 1, 1, 2.5, 2.5, 2.5, 2.5, 2.5, 2.5, 2.5])); // Output: 2 
+
+let start = performance.now()
+majorityElement([1, 2, 3, 4, 5, 5, 5, 5, 5, 5, 4, 6])
+let end = performance.now()
+
+console.log(end-start);
--- task1.js
+++ task1.js
@@ -6,44 +6,76 @@ const indonesiaCurrency = new Intl.NumberFormat('id-ID', {
 
 let start
 let end
-const date = new Date(2023, 8, 28)
-const current = new Date(2024, 11, 28)
+const date = new Date()
+const current = new Date(2023, 11, 28)
 
 let day = date.getDate()
 let month = date.getMonth()
 let year = date.getFullYear()
 
+let price, discount, tax, stock, purchase, creditTerm
+
 function monthName(month) {
     switch (month) {
-        case 1:
+        case 0:
             return "January";
-        case 2:
+        case 1:
             return "February";
-        case 3:
+        case 2:
             return "March";
-        case 4:
+        case 3:
             return "April";
-        case 5:
+        case 4:
             return "May";
-        case 6:
+        case 5:
             return "June";
-        case 7:
+        case 6:
             return "July";
-        case 8:
+        case 7:
             return "August";
-        case 9:
+        case 8:
             return "September";
-        case 10:
+        case 9:
             return "October";
-        case 11:
+        case 10:
             return "November";
-        case 12:
+        case 11:
             return "December";
         default:
             return "There is a bug in your code";
     }
 }
 
+function monthNumber(month) {
+    switch (month) {
+        case "January":
+            return 0;
+        case "February":
+            return 1;
+        case "March":
+            return 2;
+        case "April":
+            return 3;
+        case "May":
+            return 4;
+        case "June":
+            return 5;
+        case "July":
+            return 6;
+        case "August":
+            return 7;
+        case "September":
+            return 8;
+        case "October":
+            return 9;
+        case "November":
+            return 10;
+        case "December":
+            return 11;
+        default:
+            return "There is a bug in your code";
+    }
+}
 
 //Book final price using function
 function bookFinalPrice (bookPrice, bookDiscount, bookTax, amountOfStock, amountOfPurchased, termOfCredit) {
@@ -52,7 +84,7 @@ function bookFinalPrice (bookPrice, bookDiscount, bookTax, amountOfStock, amount
     const amountOfDiscount = bookPrice*bookDiscount/100
     const priceAfterTax = bookPrice + bookPrice*bookTax/100
     const amountOfTax = bookPrice*bookTax/100
-    bookPrice = bookPrice - amountOfDiscount + amountOfTax
+    bookPrice = bookPrice
     const bookTrueFalse = bookPrice < 100_000
     const stock = amountOfStock
     const purchased = amountOfPurchased
@@ -79,33 +111,9 @@ function bookFinalPrice (bookPrice, bookDiscount, bookTax, amountOfStock, amount
     }
     purchaseNote.bookStatus = bookStatus
 
-    let i = 0
-    while (i < purchased) {
-        i++
-        if(i === stock) {
-            totalPrice = i * bookPrice
-
-            purchaseNote.totalPrice = indonesiaCurrency.format(totalPrice)
-            purchaseNote.afterPurchasedStock = 0
-            if (purchaseNote.purchased > purchaseNote.stock){
-                purchaseNote.purchaseRequestLeft = purchaseNote.purchased - purchaseNote.stock
-                purchaseNote.purchaseInformation = `Your request more than our warehouse stock, please search for ${purchaseNote.purchaseRequestLeft} book left in other store`
-            } else {
-                purchaseNote.purchaseRequestLeft = 0
-                purchaseNote.purchaseInformation = `Book is out of stock`
-            }
-            break
-        } else if (i === purchased) {
-            totalPrice = i * bookPrice
-
-            purchaseNote.totalPrice = indonesiaCurrency.format(totalPrice)
-            purchaseNote.afterPurchasedStock = purchaseNote.stock - purchaseNote.purchased
-            purchaseNote.purchaseInformation = `Book is still available to purchase`
-            break
-        }
-    }
-
-    // for (let i = 0; i <= purchased; i++){
+    // let i = 0
+    // while (i < purchased) {
+    //     i++
     //     if(i === stock) {
     //         totalPrice = i * bookPrice
 
@@ -118,7 +126,6 @@ function bookFinalPrice (bookPrice, bookDiscount, bookTax, amountOfStock, amount
     //             purchaseNote.purchaseRequestLeft = 0
     //             purchaseNote.purchaseInformation = `Book is out of stock`
     //         }
-    //         console.log(purchaseNote);
     //         break
     //     } else if (i === purchased) {
     //         totalPrice = i * bookPrice
@@ -126,11 +133,39 @@ function bookFinalPrice (bookPrice, bookDiscount, bookTax, amountOfStock, amount
     //         purchaseNote.totalPrice = indonesiaCurrency.format(totalPrice)
     //         purchaseNote.afterPurchasedStock = purchaseNote.stock - purchaseNote.purchased
     //         purchaseNote.purchaseInformation = `Book is still available to purchase`
-    //         console.log(purchaseNote);
     //         break
     //     }
     // }
 
+    for (let i = 0; i <= purchased; i++){
+        if(i === stock) {
+            totalPrice = i * bookPrice
+
+            purchaseNote.totalPrice = indonesiaCurrency.format(totalPrice)
+            purchaseNote.afterPurchasedStock = 0
+            if (purchaseNote.purchased > purchaseNote.stock){
+                purchaseNote.purchaseRequestLeft = purchaseNote.purchased - purchaseNote.stock
+                purchaseNote.purchaseInformation = `Your request more than our warehouse stock, please search for ${purchaseNote.purchaseRequestLeft} book left in other store`
+            } else {
+                purchaseNote.purchaseRequestLeft = 0
+                purchaseNote.purchaseInformation = `Book is out of stock`
+            }
+            console.log(purchaseNote);
+            break
+        } else if (i === purchased) {
+            totalPrice = i * bookPrice
+
+            purchaseNote.totalPrice = indonesiaCurrency.format(totalPrice)
+            purchaseNote.afterPurchasedStock = purchaseNote.stock - purchaseNote.purchased
+            purchaseNote.purchaseInformation = `Book is still available to purchase`
+            console.log(purchaseNote);
+            break
+        }
+    }
+
+    purchaseNote.pricePerMonth = indonesiaCurrency.format(totalPrice / termOfCredit)
+
+    console.log(purchaseNote);
     let dueDate = new Array()
     for (i = 1; i <= credit; i++) {
         if (((month + i) % 12 + 1) === 1) {
@@ -143,7 +178,8 @@ function bookFinalPrice (bookPrice, bookDiscount, bookTax, amountOfStock, amount
             } else {
                 day %=29
             }
-            dueDate.push(day + " " + monthName(((month + i) % 12 + 1)) + " " + year)
+            dueDate.push({'dueDate': day + " " + monthName(((month + i) % 12)) + " " + year,
+                            'amount': purchaseNote.pricePerMonth})
             continue        
         } 
         if ((month + i) % 12 + 1 === 2 && year % 4 !== 0) {
@@ -153,35 +189,37 @@ function bookFinalPrice (bookPrice, bookDiscount, bookTax, amountOfStock, amount
             } else {
                 day %=28
             }
-            dueDate.push(day + " " + monthName(((month + i) % 12 + 1)) + " " + year)
+            dueDate.push({'dueDate': day+27 + " " + monthName(((month + i) % 12)) + " " + year,
+                            'amount': purchaseNote.pricePerMonth})
             continue
         }
-        if (((((month + i) % 12 + 1) % 2 === 1 && ((month + i) % 12 + 1) < 8)) || ((((month + i) % 12 + 1) % 2 === 0 && ((month + i) % 12 + 1) > 7))) {
+        if (((((month + i) % 12 + 1) % 2 === 1 && ((month + i) % 12) < 8)) || ((((month + i) % 12 + 1) % 2 === 0 && ((month + i) % 12 + 1) > 7))) {
             day = date.getDate()
             if (day % 31 === 0) {
                 day
             } else {
                 day %=31
             }
-            dueDate.push(day + " " + monthName(((month + i) % 12 + 1)) + " " + year)
+            dueDate.push({'dueDate': day + " " + monthName(((month + i) % 12)) + " " + year,
+                            'amount': purchaseNote.pricePerMonth})
             continue
         }
-        if (((((month + i) % 12 + 1) % 2 === 0 && ((month + i) % 12 + 1) < 8)) || ((((month + i) % 12 + 1) % 2 === 1 && ((month + i) % 12 + 1) > 7))) {
+        if (((((month + i) % 12 + 1) % 2 === 0 && ((month + i) % 12) < 8)) || ((((month + i) % 12 + 1) % 2 === 1 && ((month + i) % 12 + 1) > 7))) {
             day = date.getDate()
             if (day % 30 === 0) {
                 day
             } else {
                 day %=30
             }
-            dueDate.push(day + " " + monthName(((month + i) % 12 + 1)) + " " + year)
+            dueDate.push({'dueDate': day + " " + monthName(((month + i) % 12)) + " " + year,
+                            'amount': purchaseNote.pricePerMonth})
             continue
         }
     }
     console.log(dueDate);
 
     purchaseNote.dueDate = dueDate
-    purchaseNote.nextDueDate = monthName((month) % 12 + 1)
-    purchaseNote.pricePerMonth = indonesiaCurrency.format(totalPrice / termOfCredit)
+    purchaseNote.nextDueDate = monthName((month) % 12)
     document.getElementById("book").innerHTML = `
         <div class="relative overflow-x-auto">
             <p class="text-center"> Your notes ID: ${purchaseNote.notesId} </p>
@@ -258,7 +296,8 @@ function bookFinalPrice (bookPrice, bookDiscount, bookTax, amountOfStock, amount
             <p class="text-center">${purchaseNote.purchaseInformation}</p>
         </div>
         <div class="mt-8 md:mx-16 mx-8">            
-            <h2 class="mb-2 text-lg font-semibold text-gray-900 dark:text-white">Completed Payment:</h2>
+            <p id="payment-accumulation" class="mb-2 font-semibold text-gray-500"></p>
+            <h2 class="mb-4 text-xl font-bold text-gray-900 dark:text-white">Completed Payment:</h2>
             <ul id="due-date" class="max-w-md space-y-1 text-gray-500 list-inside dark:text-gray-400">
                 
             </ul>
@@ -266,39 +305,112 @@ function bookFinalPrice (bookPrice, bookDiscount, bookTax, amountOfStock, amount
 `
 
     let dateList = ''
+    let lastDue = purchaseNote.dueDate[purchaseNote.dueDate.length-1].dueDate.split(' ')
+    let lastDueYear = lastDue[2]
+    let lastDueMonth = lastDue[1]
+    let lastDueDay = lastDue[0]
+    let paymentAccumulation = totalPrice
+    let paymentAccumulation2 = totalPrice
+    let paymentData = new Array
+    // let thisDue
+    // let thisDueDay
+    // console.log(lastDueYear);
 
-    // console.log(date.getMonth());
-
-    for (let i = 0; i < purchaseNote.dueDate.length; i++) {
-        if (i < purchaseNote.dueDate.findIndex(
-            (element, index) => {
-                if (element.includes(monthName(current.getMonth()+1)) && element.includes(current.getFullYear()) ){
-                    return index
-                }
-            })){
+    if (lastDueYear < current.getFullYear()) {
+        for (let i = 0; i < purchaseNote.dueDate.length; i++) {
+            paymentAccumulation -= totalPrice / termOfCredit
+            paymentData.push({"date": purchaseNote.dueDate[i].dueDate, "debtLeft": paymentAccumulation})
             dateList += 
                 `
                 <li class="flex items-center">
                     <svg class="w-3.5 h-3.5 mr-2 text-green-500 dark:text-green-400 flex-shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                         <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
                     </svg>
-                    ${purchaseNote.dueDate[i]}
+                    ${purchaseNote.dueDate[i].dueDate}
                 </li>
                 `
-        } else {
-            dateList +=
-            `<li class="flex items-center">
-                <svg class="w-3.5 h-3.5 mr-2 text-gray-500 dark:text-gray-400 flex-shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
-                </svg>
-                ${purchaseNote.dueDate[i]}
-            </li>`
+        }
+    } else if(lastDueYear == current.getFullYear() && monthNumber(lastDueMonth) < current.getMonth()) {
+        for (let i = 0; i < purchaseNote.dueDate.length; i++) {
+            paymentAccumulation -= totalPrice / termOfCredit
+            paymentData.push({"date": purchaseNote.dueDate[i].dueDate, "debtLeft": paymentAccumulation})
+            dateList += 
+                `
+                <li class="flex items-center">
+                    <svg class="w-3.5 h-3.5 mr-2 text-green-500 dark:text-green-400 flex-shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
+                    </svg>
+                    ${purchaseNote.dueDate[i].dueDate}
+                </li>
+                `
+        }
+    } else if(lastDueYear == current.getFullYear() && monthNumber(lastDueMonth) == current.getMonth() && lastDueDay < current.getDate()) {
+        for (let i = 0; i < purchaseNote.dueDate.length; i++) {
+            paymentAccumulation -= totalPrice / termOfCredit
+            paymentData.push({"date": purchaseNote.dueDate[i].dueDate, "debtLeft": paymentAccumulation})
+            dateList += 
+                `
+                <li class="flex items-center">
+                    <svg class="w-3.5 h-3.5 mr-2 text-green-500 dark:text-green-400 flex-shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
+                    </svg>
+                    ${purchaseNote.dueDate[i].dueDate}
+                </li>
+                `
+        } 
+    } else {
+        for (let i = 0; i < purchaseNote.dueDate.length; i++) {
+            // thisDue = purchaseNote.dueDate[i].split(' ')
+            // thisDueDay = thisDue[0]
+            if (i < purchaseNote.dueDate.findIndex(
+                (element, index) => {
+                    if (element.dueDate.includes(monthName(current.getMonth())) && element.dueDate.includes(current.getFullYear()) ){
+                        return index
+                    }
+                })){
+                    paymentAccumulation -= totalPrice / termOfCredit
+            paymentData.push({"date": purchaseNote.dueDate[i].dueDate, "debtLeft": paymentAccumulation})
+                dateList += 
+                    `
+                    <li class="flex items-center">
+                        <svg class="w-3.5 h-3.5 mr-2 text-green-500 dark:text-green-400 flex-shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
+                        </svg>
+                        ${purchaseNote.dueDate[i].dueDate}
+                    </li>
+                    `
+            } else {
+                dateList +=
+                `<li class="flex items-center">
+                    <svg class="w-3.5 h-3.5 mr-2 text-gray-500 dark:text-gray-400 flex-shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
+                    </svg>
+                    ${purchaseNote.dueDate[i].dueDate}
+                </li>`
+            }
         }
     }
+    paymentData.forEach((_element, _index) => {
+        paymentAccumulation2 -= totalPrice/termOfCredit
+    })
+    console.log(paymentData);
+    document.getElementById("payment-accumulation").innerHTML = "Your current debt: " + indonesiaCurrency.format(Math.abs(paymentAccumulation2))
     document.getElementById("due-date").innerHTML = dateList
 }
 
-bookFinalPrice(100_000, 20, 11, 100, 10, 12)
+function submit() {
+    price = document.getElementById("price")
+    discount = document.getElementById("discount")
+    tax = document.getElementById("tax")
+    stock = document.getElementById("stock")
+    purchase = document.getElementById("purchase")
+    creditTerm = document.getElementById("credit")
+    console.log(price.value);
+    bookFinalPrice(price.value, discount.value, tax.value, stock.value, purchase.value, creditTerm.value)
+}
+
+
+// bookFinalPrice(price, discount, tax, stock, purchase, creditTerm)
 
 start = performance.now()
 // bookFinalPrice(200_000, 10, 11, 50, 4)
